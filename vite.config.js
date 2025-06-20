ae40fe604f42c32f488b0befe50e0b1dd90721df
syedhaideralizaidi <haider@gigaspheresolutions.com>
2025-06-21 00:25:27 +0500

    initial

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: '/Gigaspheresolutions/',
-  plugins: [react()],
-})
+export default defineConfig(({ command }) => {
+  const isProd = command === 'build';
+  return {
+    base: isProd ? '/gss/' : '/',
+    plugins: [react()],
+  };
+});
